f809b627027eecd64c36c282d81696f0f485ef39
Yeg-dr <141636205+Yeg-dr@users.noreply.github.com>
2025-02-11 17:28:32 +0330

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main(){
+
+    //integer (a whole number)
+
+    //int x; //declaration
+    //x = 5; //assignment
+
+    int year = 22; //combined
+    int age = 21;
+
+    //double (number including decimal (8 bytes))
+
+    double price = 12.98;
+    double temprature = 24.5;
+
+    //float (number including decimal (4 bytes))
+
+    float num = 3.5f;
+
+    //single character
+
+    char grade = 'A';
+    char dollarsign = '$';
+
+    //boolean (true or false)
+
+    bool powee = true;
+    bool forsale = true;
+
+    //string (objects that represents a sequence of text)
+
+    std::string name = "Yeg";
+    std::string day = "Wednesday";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old.";
+
+    return 0;
+}
