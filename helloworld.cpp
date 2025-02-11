fd22ff21d8652110e7b48c1ecdf32b6ec0a51745
Yeg-dr <141636205+Yeg-dr@users.noreply.github.com>
2025-02-11 17:23:29 +0330

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    //this is a comment
+    /*
+    this
+    is 
+    a
+    multi-line 
+    comment
+    */
+   
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This Is Yeg!" << '\n';
+    return 0;
+}
